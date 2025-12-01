fe80a9907fa56e29e6ab3b964d637813049bb090
Leandro Sani <leandrocs.sani@gmail.com>
2025-12-01 14:53:38 -0300

    Fix: Ativando nodejs_compat para Cloudflare Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,9 +30,7 @@ export default defineNuxtConfig({
   // CSS Global
   css: ['~/assets/css/main.css'],
 
-  nitro: {
-    preset: 'cloudflare_pages' // ou 'cloudflare' / 'cloudflare_workers' dependendo da versão
-  },
+ 
 
 
   // Variáveis de Ambiente
