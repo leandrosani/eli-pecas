ea5623715f9177f81f99c862e05c9184fd08dbfd
Leandro Sani <leandrocs.sani@gmail.com>
2025-12-01 15:47:56 -0300

    deploy cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,25 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  colorMode: { preference: 'light' },
-  app: {
-    head: {
-      link: [{
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
-      }]
-    }
-  },
+
   css: ['~/assets/css/main.css'],
+
   nitro: {
-    preset: 'cloudflare_pages',
-    compatibilityDate: '2025-07-15', // ou a data mais recente que seu Nitro / Cloudflare recomende
-    cloudflare: {
-      // chave correta para o config do Nitro
-      nodeCompat: true
-    },
+    preset: 'cloudflare-pages',
+    compatibilityDate: '2024-04-03',
+    serveStatic: true,
     prerender: {
       routes: ['/'],
       crawlLinks: true
     }
   },
+
   runtimeConfig: {
-    sessionSecret: process.env.NUXT_SESSION_SECRET,
-    appPassword: process.env.NUXT_APP_PASSWORD,
-    databaseUrl: process.env.DATABASE_URL,
+    sessionSecret: '',
+    appPassword: '',
     public: {
-      baseUrl: process.env.NUXT_PUBLIC_BASE_URL
+      baseUrl: ''
     }
   }
 })
